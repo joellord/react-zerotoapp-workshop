b6bd27a14144f5246a67b8195a4085fb558da12c
Joel Lord <joelphy@gmail.com>
2019-03-28 08:05:01 -0400

    Start of part 3

---
+++ part3/start/src/index.js
@@ -0,0 +1,8 @@
+require("./main.scss");
+
+import ReactDOM from "react-dom";
+import React from "react";
+import Hello from "./components/Hello";
+
+ReactDOM.render(<Hello name="Joel" />, document.getElementById("react-app"));
+
