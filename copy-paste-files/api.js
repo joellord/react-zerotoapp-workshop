08876ac0b1c334274214362db9ec1595c1e96613
Joel Lord <joelphy@gmail.com>
2019-03-29 11:03:28 -0400

    Tweaked the CSS Added the api data

---
+++ copy-paste-files/api.js
@@ -0,0 +1,3 @@
+const APIKEY = "c95951b6fec7e1a5b79aad4db8eaca2d";
+const city = "London, UK";
+const url = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&units=metric&cnt=7&appid=${APIKEY}`;
